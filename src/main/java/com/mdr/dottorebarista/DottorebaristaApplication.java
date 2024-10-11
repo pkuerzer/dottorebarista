dc92b3538a0722082c0e83850af35f8be22681a5
Patrick Kürzer <pkuerzer@googlemail.com>
2024-10-11 14:09:28 +0200

    init

---
+++ src/main/java/com/mdr/dottorebarista/DottorebaristaApplication.java
@@ -0,0 +1,13 @@
+package com.mdr.dottorebarista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DottorebaristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DottorebaristaApplication.class, args);
+	}
+
+}
